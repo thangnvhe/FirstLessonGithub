dce230e743adae0aff05f24d5e173def6a13558c
thangnvhe <137912183+thangnvhe@users.noreply.github.com>
2024-05-23 11:25:24 +0700

    Create SecondCode.java

---
+++ SecondCode.java
@@ -0,0 +1,7 @@
+// the second class about the print two number a , b
+public class PrintClass{
+  public static void main(String[] args){
+    int a = b = 10;
+    Sustem.out.println(a + "+" + b +"="+(a+b));
+  }
+}
